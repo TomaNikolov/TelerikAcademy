a70de1e2adc52bc6c4f570ca62ffbb7a343a1a20
TomaNikolov <on.tomas@gmail.com>
2015-01-13 19:34:58 +0200

    Commit

---
+++ C#1/Intro-Programming-Homework/CurrentDateAndTime/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace CurrentDateAndTime
+{
+    class Program
+    {
+        static void Main()
+        {
+
+           // Console.WriteLine(DateTime.Now);
+
+
+
+            Timer second = new Timer(1000);
+
+            Console.WriteLine(DateTime.Now);
+
+            second.Elapsed += OnTimedEvent;
+            second.Start();
+
+            Console.ReadLine();
+
+        }
+        private static void OnTimedEvent(Object sourse, ElapsedEventArgs e)
+        {
+            Console.Clear();
+            Console.WriteLine(DateTime.Now);
+
+        }
+    }
+}
