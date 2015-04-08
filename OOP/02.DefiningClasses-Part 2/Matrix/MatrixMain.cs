5dde48e48b7d35bde9b43ad8ba0f2dccbe3b216f
TomaNikolov <on.tomas@gmail.com>
2015-04-08 08:51:20 +0300

    Defining Classes - Part 2

---
+++ OOP/02.DefiningClasses-Part 2/Matrix/MatrixMain.cs
@@ -0,0 +1,30 @@
+﻿namespace Matrix
+{
+    using System;
+
+    public class MatrixMain
+    {
+        static void Main()
+        {
+            //Test of matrix class
+            Matrix<int> firstMatrix = new Matrix<int>(4, 4);
+            Matrix<int> secondMatrix = new Matrix<int>(4, 4);
+            for (int i = 0; i < firstMatrix.Row; i++)
+            {
+                for (int j = 0; j < firstMatrix.Col; j++)
+                {
+                    firstMatrix[i, j] = j + i + 1;
+                    secondMatrix[i, j] = j + i + 2;
+                }
+            }
+            System.Console.WriteLine(firstMatrix);
+            System.Console.WriteLine(secondMatrix);
+
+            if (firstMatrix)
+            {
+                Matrix<int> newMatrix = firstMatrix + secondMatrix;
+                System.Console.WriteLine(newMatrix);
+            }
+        }
+    }
+}
